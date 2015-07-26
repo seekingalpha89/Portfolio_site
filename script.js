6687875e04793f663be9f6fae96b692415689236
Alpha Sow <seekingalpha89@gmail.com>
2015-07-26 10:03:17 -0400

    updated portfolio

---
+++ script.js
@@ -0,0 +1,4 @@
+var logo = document.getElementById("logo");
+consol.log("This is an element of type: ", logo.nodeType);
+console.log("the Inner HTML is " ,logo.innerHTML );
+console.log("Child nodes: ", logo.childNodes.length);
