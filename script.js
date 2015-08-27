a768fed0714c3ee0955a481826eae12604296729
Alpha Sow <seekingalpha89@gmail.com>
2015-08-27 09:40:24 -0400

    updated portfolio

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-var logo = document.getElementById("logo");
-console.log(logo.innerHTML);
-
-var wrapper = document.getElementById("wrapper");
-console.log(wrapper.innerHTML);
+var logo = document.getElementById("logo"); console.log(logo.innerHTML);  var wrapper = document.getElementById("wrapper"); console.log(wrapper.innerHTML);  document.getElementById("gallery").addEventListener("click", myFunction); function myFunction() {     alert ("You are about to see Alpha's work!"); }
